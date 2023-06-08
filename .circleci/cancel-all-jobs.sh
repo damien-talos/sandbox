e7186af17b2b03d82519a8252bbb709f5c166a38
Damien Schoof <damien.schoof@talostrading.com>
2023-06-08 09:54:04 -0400

    Fix jq query again and fix cancel command

--- .circleci/cancel-all-jobs.sh
+++ .circleci/cancel-all-jobs.sh
@@ -17,7 +17,7 @@ function api-get() {
     curl -s -H "Circle-Token: ${CIRCLECI_TOKEN}" "${API_URL}/$1"
 }
 function api-post() {
-    curl -s -H "Circle-Token: ${CIRCLECI_TOKEN}" -x POST "${API_URL}/$1"
+    curl -sS -H "Circle-Token: ${CIRCLECI_TOKEN}" -X POST "${API_URL}/$1"
 }
 
 # Circle CI doesn't give any easy way to query for all running builds.
@@ -66,7 +66,7 @@ echo "${pipeline_ids}"
 
 # Breakdown of this query:
 # - Select the id of all workflows with the same name as the current workflow, are running, were created before the current workflow, and aren't the current workflow.
-jq_workflow_running_workflow_query=".items[] | select ( .name == \"${workflow_name}\" and .status == \"running\" and .id != \"${CIRCLE_WORKFLOW_ID}\" and ().created_at | fromdateiso8601 < \"${workflow_created_at}\")) | .id"
+jq_workflow_running_workflow_query=".items[] | select ( .name == \"${workflow_name}\" and .status == \"running\" and .id != \"${CIRCLE_WORKFLOW_ID}\" and (.created_at | fromdateiso8601 < ${workflow_created_at})) | .id"
 
 workflow_ids=$(echo "${pipeline_ids}" | while read -r pip_id; do
     wf_reqs=$(api-get "pipeline/${pip_id}/workflow")
